233453c2cfa0b7ab74b667789d601e2ec9705675
burak-upstash <burak@upstash.com>
2023-07-02 12:12:36 +0300

    make sure tests pass

--- tests/commands/hash/test_hvals.py
+++ tests/commands/hash/test_hvals.py
@@ -11,12 +11,10 @@ def flush_hash():
 def test_hvals():
     hash_name = "myhash"
 
-    # Add fields and values to the hash
     redis.hset(hash_name, "field1", "value1")
     redis.hset(hash_name, "field2", "value2")
     redis.hset(hash_name, "field3", "value3")
-
-    # Get all values from the hash
+    
     result = redis.hvals(hash_name)
 
-    assert result == ["value1", "value2", "value3"]  # All values from the hash
+    assert sorted(result) == sorted(["value1", "value2", "value3"])
