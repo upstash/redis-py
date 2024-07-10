9383c16d81908b2bdeda71c2b47d35ab10c667a8
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-07-10 15:59:59 +0300

    add more tests

--- tests/test_read_your_writes.py
+++ tests/test_read_your_writes.py
@@ -2,17 +2,7 @@ import pytest
 
 from upstash_redis import Redis
 
-def test_set(redis: Redis):
-    # key = "mykey"
-    # value = "myvalue"
-    # ex_seconds = 10
-    #
-    # result = redis.set(key, value, ex=ex_seconds)
-    # print('heyy!!!!')
-    # assert result is True
-    # assert redis.get(key) == value
-    # assert redis.ttl(key) == ex_seconds
-    print("LETS GO")
+def test_should_update_sync_token_on_basic_request(redis: Redis):
     redis = Redis.from_env(read_your_writes=True)
 
     initial_token = redis._upstash_sync_token
@@ -21,8 +11,48 @@ def test_set(redis: Redis):
 
     updated_token = redis._upstash_sync_token
 
-    redis.get("key")
-
-    final_token = redis._upstash_sync_token
-    print(f"'{initial_token}' - '{updated_token}' - '{final_token}'")
     assert initial_token != updated_token
+
+def test_should_update_sync_token_on_pipeline(redis: Redis):
+    redis = Redis.from_env()
+
+    initial_token = redis._upstash_sync_token
+
+    pipeline = redis.pipeline()
+
+    pipeline.set("key", "value")
+    pipeline.set("key2", "value2")
+
+    pipeline.exec()
+
+    updated_token = redis._upstash_sync_token
+
+    assert initial_token != updated_token
+
+def test_updates_after_successful_lua_script_call(redis):
+    s = """
+    redis.call('SET', 'mykey', 'myvalue')
+    return 1
+    """
+    initial_sync = redis._upstash_sync_token
+    redis.eval(
+        s, keys=[], args=[]
+    )
+
+    updated_sync = redis._upstash_sync_token
+    assert updated_sync != initial_sync
+
+def test_should_not_update_sync_state_with_opt_out_ryw():
+    redis = Redis.from_env(read_your_writes=False)
+    initial_sync = redis._upstash_sync_token
+    redis.set("key", "value")
+    updated_sync = redis._upstash_sync_token
+    assert updated_sync == initial_sync
+
+def test_should_update_sync_state_with_default_behavior():
+    redis = Redis.from_env()
+    initial_sync = redis._upstash_sync_token
+    redis.set("key", "value")
+    updated_sync = redis._upstash_sync_token
+    assert updated_sync != initial_sync
+
